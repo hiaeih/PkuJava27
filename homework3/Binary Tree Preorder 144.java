915d2f8e570effd73def7be9b949ccce51dab5ea
Angelcansmail <1845973547@qq.com>
2015-10-28 17:30:30 +0800

    第三周作业15 18 94 111 144 145树的遍历、深度的非递归、3Sum、4Sum

--- homework3/Binary Tree Preorder 144.java
+++ homework3/Binary Tree Preorder 144.java
@@ -1,4 +1,15 @@
 import java.util.*;
+/*
+Given a binary tree, return the preorder traversal of its nodes' values.
+For example:
+Given binary tree {1,#,2,3},
+   1
+    \
+     2
+    /
+   3
+return [1,2,3].
+Note: Recursive solution is trivial, could you do it iteratively?*/
 /**Ç°Ðò
  * Definition for a binary tree node.
  * public class TreeNode {
