f7a7df62ddbe5267c5dea6575f2dc3452f21ea06
gcf <gongcuifeng@pku.edu.cn>
2015-11-05 17:35:02 +0800

    作业

---
+++ homework4/169.java
@@ -0,0 +1,17 @@
+﻿public class Solution {
+    public int majorityElement(int[] nums) {
+       int element=0;
+       int count=0;
+       for(int i=0;i<nums.length;i++){
+           if(count==0){
+               element=nums[i];
+               count++;
+           }
+           else{
+               if(element==nums[i])count++;
+               else count--;
+           }
+       }
+       return element;
+    }
+}
