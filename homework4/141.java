1cc82da6cef5a8e0f55848ed9a5e9ef457e9af20
gcf <gongcuifeng@pku.edu.cn>
2015-11-06 11:41:01 +0800

    添加141至作业4

---
+++ homework4/141.java
@@ -0,0 +1,57 @@
+﻿public class Solution {
+    //方法一
+    /*public boolean hasCycle(ListNode head) {
+       if(head == null || head.next == null)
+              return false;
+         
+         ListNode Faster = head, Slower = head;
+         
+         while(Faster.next!=null && Faster.next.next!=null){
+             Slower = Slower.next;
+             Faster = Faster.next.next;
+             
+             if(Faster == Slower)
+              return true;
+         }
+         return false;
+    }*/
+    //方法二
+    public boolean hasCycle(ListNode head) {
+        ListNode slow;
+        ListNode fast;
+        
+    if(head==null)
+       return false;
+        slow=head;
+        fast=head.next;
+    while(true){
+        if(fast==null)
+           return false;
+           
+         fast=fast.next;
+         if(fast==null)
+           return false;
+         if(slow==fast)
+           return true;
+           
+        fast=fast.next;
+        if(fast==null)
+           return false;
+        if(slow==fast)
+           return true;
+           
+        slow=slow.next;
+        if(slow==fast)
+           return true;
+    }
+}
+}
+//这个类已经嵌在leetcode里了，所以不能单独再写
+// class ListNode {
+//      int val;
+//      ListNode next;
+//      ListNode(int x) {
+//          val = x;
+//          next = null;
+//      }
+//   }
