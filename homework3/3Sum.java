1e6fb9931d8306fa6e9ebf290bbdb3bafb3b9519
Angelcansmail <1845973547@qq.com>
2015-11-17 15:08:30 +0800

    fresh

--- homework3/3Sum.java
+++ homework3/3Sum.java
@@ -1,6 +1,5 @@
 import java.util.*;
 /*Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
-
 Note:
 Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ¡Ü b ¡Ü c)
 The solution set must not contain duplicate triplets.
